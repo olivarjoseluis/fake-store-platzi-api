c428d8949e520e7bc798e7d46ced3842440f031a
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-01-30 21:53:16 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar"
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <p className="italic ...">The quick brown fox ...</p>
+    </div>
+  )
+}
+
+export default App
